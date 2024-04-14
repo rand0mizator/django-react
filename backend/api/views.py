dff9c3c69159c2a81d63a1ea45fa7cd6c1048b47
Igor Suslov <iosuslov@gmail.com>
2024-04-14 17:18:05 +0700

    notes handling

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+
+from .models import Note
 from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from .serializers import NoteSerializer, UserSerializer
+
+
+class NoteListCreate(generics.ListCreateAPIView):
+
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
 
 class CreateUserView(generics.CreateAPIView):
 
