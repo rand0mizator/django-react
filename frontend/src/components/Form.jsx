87f95a7dd2c54f62fab35ac0478fce27d5f80d9b
Igor Suslov <iosuslov@gmail.com>
2024-04-17 19:26:24 +0700

    loading indicator

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -3,6 +3,7 @@ import api from "../api.js";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants.js";
 import "../styles/Form.css"
+import LoadingIndicator from "./LoadingIndicator.jsx";
 
 function Form({ route, method }) {
     const [username, setUsername] = useState("");
@@ -49,6 +50,7 @@ function Form({ route, method }) {
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Password"
             />
+            {loading && <LoadingIndicator />}
             <button className="form-button" type="submit">
                 {name}
             </button>
